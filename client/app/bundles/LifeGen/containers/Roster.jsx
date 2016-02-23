6004e77c61dcd240ba9b3cb1566119c27df0a10e
Damon Black <black.damon@gmail.com>
2016-02-23 17:23:26 -0500

    Display some rudimentary data for character stats

--- client/app/bundles/LifeGen/containers/Roster.jsx
+++ client/app/bundles/LifeGen/containers/Roster.jsx
@@ -3,13 +3,20 @@ import React, { PropTypes } from 'react';
 export default class LifeGen extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = { name: this.props.lives };
   }
 
   render() {
+    var lives = this.props.lives;
     return (
       <div>
+        {lives.map(function(life) {
+          return (
+            <div>
+              <h3>{life.character_sheet.name}</h3>
+              <div>{JSON.stringify(life.character_sheet.attributes)}</div>
+            </div>
+          );
+        })}
       </div>
     );
   }
